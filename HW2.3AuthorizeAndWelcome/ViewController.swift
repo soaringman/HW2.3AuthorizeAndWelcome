ff4813cb73b72cc3e8b4361d71abfc36b9de4c23
Alexey Gulyaev <ghost-shadows@yandex.ru>
2021-12-07 15:07:53 +0300

    Initial Commit

---
+++ HW2.3AuthorizeAndWelcome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2.3AuthorizeAndWelcome
+//
+//  Created by Алексей Гуляев on 07.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
