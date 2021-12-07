705e5bf476a77e05bdb5fdaf73ad4926860974e2
Alexey Gulyaev <ghost-shadows@yandex.ru>
2021-12-07 15:34:25 +0300

    Add main object on AuthorizeViewController

---
+++ HW2.3AuthorizeAndWelcome/WeclomeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WeclomeViewController.swift
+//  HW2.3AuthorizeAndWelcome
+//
+//  Created by Алексей Гуляев on 07.12.2021.
+//
+
+import UIKit
+
+class WeclomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+ 
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
